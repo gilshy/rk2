ab6aa8643397d02dd39634d06e16579e1a4e14f7
gilshy <114899766+gilshy@users.noreply.github.com>
2022-11-14 11:09:07 +0300

    Update rk2_1.py

--- rk2_1.py
+++ rk2_1.py
@@ -1,5 +1,6 @@
 """counting IMT"""
-weight = int(input('Type your weight: '))
-height = int(input('Type your height: '))
-imt = round(weight/(height**2), 5)
-print(imt)
+if __name__ == "__main__":
+    weight = int(input('Type your weight: '))
+    height = int(input('Type your height: '))
+    imt = round(weight/(height**2), 5)
+    print(imt)
